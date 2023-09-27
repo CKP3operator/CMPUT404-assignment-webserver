6401e24c63c260a4503e6a2bee9a7c19f2b84599
CKP3operator <perezcharles1112@gmail.com>
2023-09-26 23:50:19 -0600

    All test passing but css is not being served properly

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 #  coding: utf-8 
 import socketserver
 import os
+import mimetypes
 # Copyright 2013 Abram Hindle, Eddie Antonio Santos
 # 
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -26,45 +27,67 @@ import os
 
 # try: curl -v -X GET http://127.0.0.1:8080/
 
-folderServe = os.path.join(os.path.dirname(__file__), 'www')
+base_dir = './www'
 
 class MyWebServer(socketserver.BaseRequestHandler):
     
     def handle(self):
         self.data = self.request.recv(1024).strip()
         print ("Got a request of: %s\n" % self.data)
-        #self.request.sendall(bytearray("OK",'utf-8'))
-        request_lines = self.data.decode().split('\r\n')
-        method,path,protocol = request_lines[0].split()
-        if method != 'GET':
-            response = f"HTTP/1.1 405 Method Not Allowed\r\n\r\n<h1>405 Method Not Allowed</h1>"
-            self.request.sendall(response.encode())
-        elif method == 'GET' and path != '/' and os.path.isdir(os.path.join(folderServe, path.lstrip('/'))) and not path.endswith('/'):
-            new_location = path + '/'
-            response = f"HTTP/1.1 301 Moved Permanently\r\nLocation: {new_location}\r\n\r\n"
-            self.request.sendall(response.encode())
+        request_data = self.data.decode("utf-8").splitlines()
+        method,path, _ = request_data[0].split()
+        abs_path = os.path.abspath(os.path.join(base_dir,path[1:]))
+        if not abs_path.startswith(os.path.abspath(base_dir)):
+            body= b"404 Not Found"
+            headers = "HTTP/1.1 404 Not Found\r\nContent-Type: text/plain\r\nContent-Length: 12\r\n\r\n"
+            response = headers.encode('utf-8') + body
+            self.request.sendall(response)
+        elif method != "GET":
+            body = b"405 Method Not Allowed"            
+            headers = "HTTP/1.1 405 Method Not Allowed\r\nContent-Type: text/plain\r\nContent-Length: 21\r\n\r\n"
+            response = headers.encode('utf-8') + body
+            self.request.sendall(response)
         else:
-            if path == '/':
-                request_path = os.path.join(folderServe, 'index.html')
-                if os.path.exists(request_path) and os.path.isfile(request_path):
-                    with open(request_path, 'rb') as file:
-                        response_data = file.read()
-                    response = f"HTTP/1.1 200 OK\r\nContent-Length: {len(response_data)}\r\n\r\n"
-                    self.request.sendall(response.encode() + response_data)
-                else:
-                    response = "HTTP/1.1 404 Not Found\r\n\r\n<h1>404 Not Found</h1>"
-                    self.request.sendall(response.encode())
             
-            else:
-                request_path = os.path.join(folderServe, path.lstrip('/'))
-                if os.path.exists(request_path) and os.path.isfile(request_path) and request_path.startswith(folderServe):
-                    with open(request_path, 'rb') as file:
-                        response_data = file.read()
-                    response = f"HTTP/1.1 200 OK\r\nContent-Length: {len(response_data)}\r\n\r\n"
-                    self.request.sendall(response.encode() + response_data)
+            if os.path.isdir(abs_path) and not path.endswith('/'):
+                location = f"http://{self.server.server_address[0]}:{self.server.server_address[1]}{path}/"
+                body = b"301 Moved Permanently"
+                headers = f"HTTP/1.1 301 Moved Permanently\r\nContent-Type: text/plain\r\nContent-Length: 21\r\nLocation: {location}\r\n\r\n.encode('utf-8')"
+                response = headers.encode('utf-8') + body
+                self.request.sendall(response)
+            elif os.path.isdir(abs_path):
+                index = os.path.join(abs_path, "index.html")
+                content_type, _ = mimetypes.guess_type(index)
+                
+                if os.path.exists(index) and os.path.isfile(index):
+                    with open(index, 'rb') as file:
+                        body = file.read()
+                    headers = f"HTTP/1.1 200 OK\r\nContent-Type: {content_type}\r\nContent-Length: {len(body)}\r\n"
+                    response = headers.encode('utf-8') + body
+                    self.request.sendall(response)
                 else:
-                    response = "HTTP/1.1 404 Not Found\r\n\r\n<h1>404 Not Found</h1>"
-                    self.request.sendall(response.encode())
+                    body = b"404 Not Found"
+                    headers = "HTTP/1.1 404 Not Found\r\nContent-Type: text/plain\r\nContent-Length: 12\r\n\r\n"
+                    response = headers.encode('utf-8') + body
+                    self.request.sendall(response)
+            else:
+                content_type, _ = mimetypes.guess_type(abs_path)
+                
+                if os.path.exists(abs_path) and os.path.isfile(abs_path):
+                    with open(abs_path, 'rb') as file:
+                        body = file.read()
+                    headers = f"HTTP/1.1 200 OK\r\nContent-Type: {content_type}\r\nContent-Length: {len(body)}\r\n"
+                    response = headers.encode('utf-8') + body
+                    self.request.sendall(response)
+                else:
+                    body = b"404 Not Found"
+                    headers = "HTTP/1.1 404 Not Found\r\nContent-Type: text/plain\r\nContent-Length: 12\r\n\r\n"
+                    response = headers.encode('utf-8') + body
+                    self.request.sendall(response)
+
+
+        
+        
 if __name__ == "__main__":
     HOST, PORT = "localhost", 8080
 
@@ -74,4 +97,7 @@ if __name__ == "__main__":
 
     # Activate the server; this will keep running until you
     # interrupt the program with Ctrl-C
-    server.serve_forever()
+    try:
+        server.serve_forever()
+    except KeyboardInterrupt:
+        server.server_close()
